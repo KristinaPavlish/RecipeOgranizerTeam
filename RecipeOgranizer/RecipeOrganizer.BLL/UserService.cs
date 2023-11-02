8a0d9a7fe0fe6fb99eff27a52562a7edadfe2242
sashka.olashyn <sashka.olashyn@gmail.com>
2023-11-02 20:01:40 +0200

    login functionality and page added

---
+++ RecipeOgranizer/RecipeOrganizer.BLL/UserService.cs
@@ -0,0 +1,34 @@
+﻿namespace RecipeOrganizer.BLL;
+
+
+using RecipeOgranizer.Dal.Context;
+using RecipeOgranizer.Dal.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+public class UserService
+{
+    private RecipeOrganizerContext _context;
+
+    public UserService(RecipeOrganizerContext context)
+    {
+        _context = context;
+    }
+
+    public User GetUserByEmail(string email)
+    {
+        return _context.Users.FirstOrDefault(u => u.Email == email);
+    }
+
+    public bool AuthenticateUser(string email, string password)
+    {
+        var user = GetUserByEmail(email);
+
+        if (user != null && user.Userpassword == password)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ RecipeOgranizer/View/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using RecipeOgranizer.Dal.Context;
+using RecipeOrganizer.BLL;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+            using (RecipeOrganizerContext context = new RecipeOrganizerContext())
+            {
+                UserService userService = new UserService(context);
+
+                if (userService.AuthenticateUser(username, password))
+                {
+                    MessageBox.Show("Успішно авторизовано.");
+                }
+                else
+                {
+                    MessageBox.Show("Не вдалося авторизуватися. Перевірте ім'я користувача та пароль.");
+                }
+            }
+        }
+    }
+}
